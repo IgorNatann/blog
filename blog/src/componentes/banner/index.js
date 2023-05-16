8d9ce06843853df3e74c659074fb9ce8b490ad45
Igor Natan <igornatan4@gmail.com>
2023-05-16 16:22:42 -0300

    commit component banner

--- blog/src/componentes/banner/index.js
+++ blog/src/componentes/banner/index.js
@@ -1,9 +1,11 @@
 import styles from './Banner.module.css'
+import circuloColorido from '../../assets/circulo_colorido.png'
+import minhaFoto from '../../assets/minha_foto.jpeg'
 import React from 'react'
 
 export default function Banner() {
     return (
-        <div className={styles.Banner}>
+        <div className={styles.banner}>
             <div className={styles.apresentacao}>
                 <h1 className={styles.titulo}>
                     Olá, Mundo!
@@ -12,8 +14,17 @@ export default function Banner() {
                     Boas vindas ao meu espaço pessoal! Eu sou Igor Natan, Tenho 23 anos! Sou formado em Analise e desenvolvimento de Sistemas. Sou desenvolvedor Front-end. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
                 </p>
             </div>
-            <div className={styles.Banner}>
-
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt='Foto do Igor Natan olhando para o lado esquerdo.'
+                />
             </div>
         </div>
     )
