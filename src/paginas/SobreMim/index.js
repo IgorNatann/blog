a1c59b985937d4d25394932cc2cc096294296d09
Igor Natan <igornatan4@gmail.com>
2023-05-18 14:32:53 -0300

    commit component Post

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,13 @@
+import PostModelo from "componentes/postModelo";
+import fotoCapa from "assets/sobre_mim_capa.png"
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim...</h1>
+       <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+       >
+        conteudo..
+       </PostModelo>
     )
 }
