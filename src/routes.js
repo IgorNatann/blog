eac48054b3409621d7d173374b94dc3767aba1eb
IgorNatann <igornatan4@gmail.com>
2023-06-28 15:22:27 -0300

    commit component posts

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Rodape from "componentes/rodape";
 import PaginaPadrao from "componentes/paginaPadrao";
+import Post from "paginas/Post";
 
 function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
 
         <Route path="*" element={<div>Página não encontrada</div>} />
