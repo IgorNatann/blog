989f02aa2ed1b9e3e5efa1cd72f81548bb711844
IgorNatann <igornatan4@gmail.com>
2023-06-28 14:40:57 -0300

    commit componente sobremim alteração texto

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -18,34 +18,30 @@ export default function SobreMim() {
 
             <img
                 src={fotosSobreMim}
-                alt="Foto do Igor Natan caminhando e olhando para o lado direito"
+                alt="Foto do Igor Natan sorrindo"
                 className={styles.fotoSobreMim}
             />
 
             <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+                Oi, tudo bem? Eu sou desenvolvedor Front-end e estou feliz de te ver por aqui.
             </p>
 
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
-
+                Minha história com programação começou por volta de 2017, onde estava terminando o ensino medio e precisava definir um norte para seguir. Sempre gostei de hardware, mas não tinha tanto contato com a programação em si. Após realizar um teste vocacional, onde o mesmo me forneceu duas opções de possíveis profissões que me encaixaria, a primeira sendo Programação e em segundo Ed.Física.. Optei por Programação e até o momento não me arrependo nem um pouco dessa escolha, e só tenho a agradecer esse universo maravilhoso da tecnologia.
             </p>
 
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
+                Hoje eu sou formado em Analise e desenvolvimento de sistemas, trabalho na área a aproximadamente 3 anos.
             </p>
 
             <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
+                Ainda não estou exercendo a função de desenvolvedor Web, mas cada vez mais estou apromirando meus conhecimento e colocando em pratica em projetos pessoais.
             </p>
 
             <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+                Desenvolvi esse blog a fins de expansão de conhecimento.
             </p>
 
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
-            </p>
         </PostModelo>
     )
 }
